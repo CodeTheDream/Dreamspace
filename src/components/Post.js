d2fe7dc07f8d6684e270363b6e11b900ce63b73d
Ella Abraham <48738996+EllaAb@users.noreply.github.com>
2019-12-09 22:12:12 -0600

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -11,23 +11,37 @@ class Post extends React.Component {
 
 
          return (
-            
+          
              <div className="wrapper">
-                 <div  className="NewPost">Create Post  <a href="#" className="right" >Link</a> </div>
+                 <div className="NewPost"  >Create Post<a  className="right" href="#" > Link</a></div>
+                 <div className="subgrid-post">
                  
                  { PostsData.map((postDetail,index) => {
                  return (
                    <div className="PostedBy" >  
                  <p className="Start">{postDetail.postedBy }  { postDetail.timeAt}</p>
+                
                  
                        <p className="Topic">{postDetail.content}</p>
+                       <div class="buttonGrid" >
+                       <button  type="button" >Comment</button>
+                       <div className="nested">
+                       <button type="button" >Share</button>
+                       <button  type="button" >Save</button>
                        </div>
+                        </div>
+                       </div>
+    
                     
-                 );
-                 })}
+                       );
+                   })}
+                   
+                </div>
              </div>
+             
+             
          );
-    }
+      }
 
    
 }
