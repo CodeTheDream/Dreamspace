498aa45404fc8a9ce2e1d98400bc295dde6fe22d
aster <rtesagg84@gmail.com>
2019-12-23 00:51:13 -0500

    this is my second commit

--- src/containers/Create-article/index.js
+++ src/containers/Create-article/index.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
-import axios from "axios";
+import { compose } from "recompose";
+import {
+  AuthUserContext,
+  withAuthorization,
+  withEmailVerification
+} from "../../components/Session";
+import { withFirebase } from "../../components/Firebase";
 import "./create_article.css";
 const options = ["Select Tag", "React", "Ruby", "Javascript"];
-
-
+const moment = require('moment');
 class Dialog extends React.Component {
   render() {
     return (
-      <div className='dialogstyle '>
-        <button className=" dialogCloseButonStayle" onClick={this.props.closePopup}> X </button>
-        <div className='diglog_inner'>
+      <div className="dialogstyle ">
+        <button
+          className=" dialogCloseButonStayle"
+          onClick={this.props.closePopup}
+        >
+          X
+        </button>
+        <div className="diglog_inner">
           <h4>{this.props.children}</h4>
-        
         </div>
       </div>
     );
@@ -23,152 +32,152 @@ class Createarticle extends Component {
     this.state = {
       title: "",
       description: "",
-      value: "Select an Option",
-      urlBody: ""
+      tags: "Select an Option",
+      url: "",
+      downvotes: 0,
+      upvotes: 0,
+      authorID: "",
+      timeCreated: ""
     };
   }
+  
   togglePopup() {
     this.setState({
       showPopup: !this.state.showPopup
     });
   }
-
-
   onUrlChange = e => {
     this.setState({
-      urlBody: e.target.value
+      url: e.target.value
     });
   };
-
   onTagChange = e => {
     this.setState({
-      value: e.target.value
+      tags: e.target.value
     });
   };
-
   onTitleChange = e => {
     this.setState({
       title: e.target.value
     });
   };
-
   onBodyChange = e => {
     this.setState({
       description: e.target.value
     });
   };
-  handleSubmit = e => {
+  handleSubmit = (e, authUser) => {
+    
+ 
+     
     e.preventDefault();
-    const data = {
+
+    this.props.firebase.articles().add({
+      userId: authUser.uid,
       title: this.state.title,
       description: this.state.description,
-      value: this.state.value,
-      urlBody: this.state.urlBody
-    };
-    axios
-      .post("https://jsonplaceholder.typicode.com/posts", data)
-      .then(res => console.log(res))
-      .catch(err => console.log(err));
-  };
-
-  /* resetForm = () => {
-    this.setState({
-      title: "",
-      description: "",
-      value: "",
-      urlBody:""
+      tags: this.state.tags,
+      url: this.state.url,
+      downvotes: this.state.downvotes,
+      upvotes: this.state.upvotes,
+      timeCreated:moment().format(` MMMM DD, YYYY  --  hh:mm:ss A  UTC-6`) 
+      //timeCreated:moment().add(moment().utcOffset(), 'm').utc().format()
+      
     });
-  };*/
-  resetform = () => {
-    window.location.reload(false);
+
+    this.setState({
+      tags: "",
+      title: "",
+      url: "",
+      description: "",
+      downvotes: 0,
+      upvotes: 0
+    });
   };
   render() {
     return (
+      <AuthUserContext.Consumer>
+         {authUser => (
       <div>
-        <input placeholder="create_article" onClick={this.togglePopup.bind(this)}/> 
-
-{this.state.showPopup ?
-  <Dialog
-
-   closePopup={this.togglePopup.bind(this)}
-  >
-      
-      <div className=" Datapost-form">
-        <div className="grid-container">
-          <div className="subgrid-container1">Create a new post</div>
-
-        {/*}  <div className="subgrid-container2">Today Top Growing Taps</div>*/}
-          <div className="form-area">
-            <div className="subgrid-container3">
-              <ul >
-                <li>
-                  <form className="subgrid-post" onSubmit={this.handleSubmit}>
-                    <ul >
-                     
-                      <li >
-                        <label>Tags</label>
-
-                        <select
-                          value={this.state.value}
-                          onChange={this.onTagChange}
+        <input
+          placeholder="create_article"
+          onClick={this.togglePopup.bind(this)}
+        />
+        {this.state.showPopup ? (
+          <Dialog closePopup={this.togglePopup.bind(this)}>
+            <div className=" Datapost-form">
+              <div className="grid-container">
+                <div className="subgrid-container1">Create a new post</div>
+                <div className="form-area">
+                  <div className="subgrid-container3">
+                    <ul>
+                      <li>
+                        <form
+                        
+                          className="subgrid-post"
+                          onSubmit={e =>this.handleSubmit(e, authUser)}
                         >
-                          {options.map(option => {
-                            return (
-                              <option value={option} key={option}>
-                                {option}
-                              </option>
-                            );
-                          })}
-                        </select>
+                          <ul>
+                            <li>
+                              <label>Tags</label>
+                              <select
+                                value={this.state.tags}
+                                onChange={this.onTagChange}
+                              >
+                                {options.map(option => {
+                                  return (
+                                    <option value={option} key={option}>
+                                      {option}
+                                    </option>
+                                  );
+                                })}
+                              </select>
+                            </li>
+                            <li>
+                              <input
+                                placeholder="Title"
+                                value={this.state.title}
+                                onChange={this.onTitleChange}
+                                required
+                              />
+                            </li>
+                            <li>
+                              <textarea
+                                placeholder="URl"
+                                value={this.state.url}
+                                onChange={this.onUrlChange}
+                              />
+                            </li>
+                            <li>
+                              <textarea
+                                col={30}
+                                rows={10}
+                                placeholder="Description"
+                                value={this.state.description}
+                                onChange={this.onBodyChange}
+                                required
+                              />
+                            </li>
+                            <li>
+                              <button type="submit"> Post</button>
+                            </li>
+                          </ul>
+                        </form>
                       </li>
-                   
-                      <li >
-                        <input
-                          placeholder="Title"
-                          value={this.state.title}
-                          onChange={this.onTitleChange}
-                          required
-                        />
-                      </li>
-                      <li >
-                        <textarea
-                          placeholder="URl"
-                          value={this.state.urlBody}
-                          onChange={this.onUrlChange}
-                        />
-                      </li>
-                      <li >
-                        <textarea
-                          col={30}
-                          rows={10}
-                          placeholder="Description"
-                          value={this.state.description}
-                          onChange={this.onBodyChange}
-                          required
-                        />
-                      </li>
-                      
-                        <li>
-                          <button type="submit"> Post</button>
-                        </li>
-                     
                     </ul>
-                  </form>
-                </li>
-              </ul>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-          {/*<div>4</div>*/}
-        </div>
+          </Dialog>
+        ) : null}
       </div>
-       
-      </Dialog>
-         : null
-       }
-      </div>
+      )}
+      </AuthUserContext.Consumer>
     );
+    
   }
+
 }
 
-export default Createarticle;
-
+export default compose(withFirebase)(Createarticle);
