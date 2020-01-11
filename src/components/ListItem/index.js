a6ba143b9446b54fd203e2f0502e6c02dda6dad4
Elsabeth Kidane <elizit88@gmail.com>
2020-01-10 19:59:51 -0500

    commitment

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -11,6 +11,7 @@ class ListItem extends React.Component {
             article: [],
             upvotes: [],
             downvotes: [],
+<<<<<<< HEAD
             calculatedvote: 0,
             // disabled: false
         };
@@ -152,13 +153,126 @@ class ListItem extends React.Component {
                                     <button
                                         onClick={(e) => this.handleUpvote(e, authUser)}
                                         disabled={this.props.article.upvote > 6 ? true : false}>                                                        <i className="fa fa-arrow-up custom"> </i>
+=======
+            calculatedvote: 0
+        };
+        //this.handleVote = this.handleVote.bind(this);
+    }
+    // onClick = {(e) => this.handleVote(e, authUser)}>
+    componentDidMount() {
+        const { article } = this.props
+        let upvotes = article.upvotes;
+        let downvotes = article.downvotes;
+        this.calculatedvote(upvotes, downvotes)
+    }
+    calculatedvote(upvotes, downvotes) {
+        if (upvotes == 0) {
+            upvotes = []
+        }
+        if (downvotes == 0) {
+            downvotes = []
+        }
+        console.log("upvote", upvotes)
+        console.log("downvote", downvotes)
+      let upvotesTotal = upvotes.length;
+      let downvotesTotal = downvotes.length;
+      let finalTotal = upvotesTotal - downvotesTotal;
+        console.log("upvotestotal", upvotesTotal)
+        console.log("downvotestotal", downvotesTotal)
+        this.setState({ calculatedvote: finalTotal })
+
+    }
+    handleUpvote = (e, authUser) => {
+
+        const { article } = this.props
+        let initialvote = [authUser.uid];
+        // console.log("article", article)
+        if (article.upvotes == 0) {
+            this.props.firebase
+                .article(article.uid)
+                .set({
+                    ...article,
+                    upvotes: initialvote
+                })
+        }
+        
+
+        else {
+            this.setState({ calculatedvote: this.state.calculatedvote + 1 })
+            let upvotes = article.upvotes
+            let updatedUpvotes = upvotes
+            updatedUpvotes.push(authUser.uid)
+            // console.log("article2", article)
+            this.props.firebase
+                .article(article.uid)
+                .set({
+                    ...article,
+                    upvotes: updatedUpvotes
+                })
+
+        }
+    }
+    handleDownvote = (e, authUser) => {
+        const { article } = this.props
+        let initialvote = [authUser.uid];
+        // console.log("article", article)
+        if (article.downvotes == 0) {
+            this.props.firebase
+                .article(article.uid)
+                .set({
+                    ...article,
+                    downvotes: initialvote
+                })
+        }
+        else {
+            this.setState({ calculatedvote: this.state.calculatedvote - 1 })
+            let downvotes = article.downvotes
+            let updatedDownvotes = downvotes
+            updatedDownvotes.push(authUser.uid)
+            // console.log("article2", article)
+            this.props.firebase
+                .article(article.uid)
+                .set({
+                    ...article,
+                    downvotes: updatedDownvotes
+                })
+            //console.log("currentuser", authUser)
+            //console.log("upvotes", this.state.calculatedvote)
+
+        }
+    }
+    render() {
+        // const { upvotes } = this.state
+        // const { downvotes } = this.state
+        const { countedvote } = this.state
+        return (
+            <AuthUserContext.Consumer>
+                {
+                    authUser => (
+                        <div className="posts">
+                            <div
+                                className="likes"
+                                style={{
+                                    width: "40px; border-left:4px solid transparent;",
+                                    float: "left"
+                                }}
+                            >
+                                <span style={{ fontSize: "1em" }}>
+                                    <button
+                                        onClick={(e) => this.handleUpvote(e, authUser)}>
+
+                                        <i className="fa fa-arrow-up custom"> </i>
+>>>>>>> ee2c958bdc0355d7ebf3a62ec93cca990cc86e5b
                                     </button>
                                     <br />
                                     {this.state.calculatedvote}
                                     <br />
                                     <button
                                         onClick={(e) => this.handleDownvote(e, authUser)}>
+<<<<<<< HEAD
 
+=======
+>>>>>>> ee2c958bdc0355d7ebf3a62ec93cca990cc86e5b
                                         <i className="fa fa-arrow-down custom"></i>
                                     </button>
                                 </span>
@@ -195,4 +309,8 @@ class ListItem extends React.Component {
         )
     }
 }
+<<<<<<< HEAD
 export default compose(withFirebase, withRouter)(ListItem);
+=======
+export default compose(withFirebase, withRouter)(ListItem);
+>>>>>>> ee2c958bdc0355d7ebf3a62ec93cca990cc86e5b
