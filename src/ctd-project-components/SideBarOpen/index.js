b0981823725e9a72fb08e770ed0d6f9e54757c2e
Nicholas Santiago <santiago.nicholas.a@gmail.com>
2020-03-12 10:01:28 -0400

    sidebar filter, sidebar search filter, css searchbox

--- src/ctd-project-components/SideBarOpen/index.js
+++ src/ctd-project-components/SideBarOpen/index.js
@@ -6,6 +6,12 @@ import SearchBar from '../SearchBar';
 
 const SideBarOpen = props => {
   let sideBarMenu = props.filterProject
+  let probableList = sideBarMenu.filter(filterProjectList =>
+    filterProjectList.fields.Status !== 'Probable'
+    );
+
+    let filteredList = probableList.filter(project => 
+      project.fields.Status !== 'Complete')
   // const dummyList = props.projectData;
   // console.log("dummy", dummyList);
   // const probableList = dummyList.filter(
@@ -18,35 +24,47 @@ const SideBarOpen = props => {
   // let filterSideBarMenu = props.filterProject
   // console.log('filterSideBarMenu ', filterSideBarMenu)
   console.log('sideBarMenu ', sideBarMenu)
+  console.log('probableList ', probableList)
+  console.log('filteredList ', filteredList)
+
+  // let actualProbableList = probableList;      // figure how to add only probable projects
+  // let notProbableList = sideBarMenu;          // need to somehow link them conditionally or something?
+  // console.log('actualProbableList ', probableList)
+  // console.log('notProbableList ', notProbableList)
+
+  
+    return (
+      <div className="sidebar-menu">
+        <div className="btn-home">
+          {/* <OpenClose /> */}       {/* I don't need at this time or if at all */}
+          <div className = 'search-container'>
+                <SearchBar
+                  projectData={props.projectData}
+                  selectProject={props.selectProject}
+                  handleInput={props.handleInput}
+                />
+            </div>
+              
+            
+        </div>
+        <div className = 'menu-container'>
+        {filteredList.map(sideBar => (
+          <li
+            key={sideBar.fields.Name}
+            onClick={() => props.selectProject(sideBar.id)}
+            className="menu-container"
+          >
+            {sideBar.fields.Name.toUpperCase()}
+          </li>
+        ))}
+        </div>
+      </div>
+    );
+
+    
+  };
+
 
   // console.log("check for today's ", sideBarMenu);
-  return (
-    <div className="sidebar-menu">
-      <div className="btn-home">
-        {/* <OpenClose /> */}       {/* I don't need at this time or if at all */ }
-        <div className = 'search-container'>
-              <SearchBar
-                projectData={props.projectData}
-                selectProject={props.selectProject}
-                // handleInput={props.handleInput}
-              />
-          </div>
-            
-          
-      </div>
-      <div className = 'menu-container'>
-      {sideBarMenu.map(sideBar => (
-        <li
-          key={sideBar.fields.Name}
-          onClick={() => props.selectProject(sideBar.id)}
-          className="menu-container"
-        >
-          {sideBar.fields.Name.toUpperCase()}
-        </li>
-      ))}
-      </div>
-    </div>
-  );
-};
 
 export default SideBarOpen;
