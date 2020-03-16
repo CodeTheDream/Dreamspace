befac95ea3e4b660ae6ab4c15558db6ee3fe7410
Nicholas Santiago <santiago.nicholas.a@gmail.com>
2020-03-16 12:26:50 -0700

    test

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import DayOfWeek from '../../ctd-project-components/DayOfWeek';
 
 
 const Footer = () => (
     <div className="footer">
         <div className="footer-content">Copyright ©{new Date().getFullYear()} Code the Dream</div>
+        <DayOfWeek />
     </div>
+
 )
 
 export default Footer
---
+++ src/ctd-project-components/DayOfWeek/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class DayOfWeek extends React.Component {
+
+  render() {
+    let date = new Date();
+    let weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    let getTheDayOfTheWeek = weekday[date.getDay()];
+    return(
+      <div className = 'today'>
+        <p className = 'style-day'>{getTheDayOfTheWeek}</p>
+      </div>
+    )
+  }
+}
+
+export default DayOfWeek;
