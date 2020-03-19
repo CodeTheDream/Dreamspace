7c7ff3d96132c4eae800b074f5399368946856ab
aster <rtesagg84@gmail.com>
2020-03-19 19:02:03 -0400

    new update

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -105,7 +105,7 @@ class SignUpFormBase extends Component {
       username === "";
 
     return (
-      <div className="view-container">
+      <div className="view-container-signup">
       <form onSubmit={this.onSubmit}  className="devedit-form" >
         <fieldset >
         <input
