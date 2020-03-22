dfbec29d5e47cfcff9a8e11f072fe536cfb78ffb
Elsabeth Kidane <elizit88@gmail.com>
2020-03-22 12:47:04 -0400

    vote update

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -18,7 +18,7 @@ class Dashboard extends React.Component {
     this.state = {
       articles: [],
         search: "" ,
-        sortType: "asc",
+       sortType: "dsc",
       calculatedvote:[],
     };
   }
@@ -26,7 +26,7 @@ class Dashboard extends React.Component {
     componentDidMount() {
         // let articles =this.props.firebase.articles()
         this.unsubscribe = this.props.firebase.articles()
-            .orderBy('timeCreated','asc')
+           // .orderBy('timeCreated','asc')
             .onSnapshot(snapshot => {
             let articles = [];
             snapshot.forEach(doc => articles.push({ ...doc.data(), uid: doc.id }));
@@ -68,13 +68,13 @@ class Dashboard extends React.Component {
       )
         });
         
-       /* if (filteredArticles) {
+        if (filteredArticles) {
             filteredArticles.sort((a, b) => {
-                const isReversed = (sortType === 'dsc') ? 1 : -1;
+                const isReversed = (sortType === 'asc') ? 1 : -1;
                 return isReversed * a.timeCreated.localeCompare(b.timeCreated)
             })
 
-        }*/
+        }
       
 
       
