9f14acd7ce9aaa58a01e650897da62f6df7d1ac2
Nicholas Santiago <santiago.nicholas.a@gmail.com>
2020-03-27 15:43:34 -0400

    errorssss

--- src/ctd-project-components/DirectoryList/index.js
+++ src/ctd-project-components/DirectoryList/index.js
@@ -1,63 +1,55 @@
 import React from 'react';
+import DirectorySearchBar from '../DirectorySearchBar';
 
 const DirectoryList = (props) => {
   // all ctd staff info
   let staffList = props.crewDirectory
   console.log('staffNames ', staffList);
+  let filteredOptInList = props.filterDirectory;
+  console.log('filteredOptInList ', filteredOptInList)
   // opt in members list
   let optInStaffMembers = staffList.filter(filterList  => 
     filterList.fields.Directory === "YES, Include my contact info in CTD Team Directory")
     console.log("optInStaffMembers ", optInStaffMembers);
   let getImage = optInStaffMembers.map(( renderImage ) => renderImage.fields.Photo);
   console.log('getImage ', getImage)
-  // const array = Object.keys(getImage);
-  // console.log('array', array)
-  // const putListInOrder = getImage.keys()
-  // for (const key of putListInOrder) {
-  //   console.log('key ', key)
-  // }
-  // let test = getImage[0];
-  // console.log('test', test)
-  // const pleaseWork = getImage.map((post) => post.filename);
-  // console.log('pleaseWork ', pleaseWork)
-  // const tryList = getImage.entries()
-  // console.log('tryList ', tryList.next().value);
-  // const digDeeper = getImage.map(muchDeeper => muchDeeper.filename)
-  // console.log('digDeeper ', digDeeper);
-  // let grabPhoto = optInStaffMembers.fields.Photo[0].url
-  // console.log('grabPhoto ', grabPhoto)
-  
-    return(
-      <div className = 'list-container'>
-      {/* {getImage.map(getImageDetails => (
-        <img 
-          className = 'images'
-          // key = {getImageDetails}
-          src = {getImageDetails.url} />
-      ))} */}
-        {optInStaffMembers.map(renderTheList => (
-          <ul
-            key = {getImage['filename']}
-            className = 'list'>
-              <img className = 'image-contain'
-                   src = {getImage['filename']}
-                   alt = 'Staff Photos'></img>
-                   {console.log('test key ', getImage['filename'])}
-              <li>{renderTheList.fields.Name.toUpperCase()}</li>
-              {/* <li>{renderTheList.fields['Slack Name'].toUpperCase()}</li> */}
-              <li>{renderTheList.fields.Phone}</li>
-              <li>{renderTheList.fields['Email address']}</li>
-
-            </ul>
-          
-        ))}
-       
-        
-      </div>
+  return(
+    <div className = 'list-container'>
+      {optInStaffMembers.map(staff => {
+        let pics = 'https://ya-webdesign.com/images250_/placeholder-image-png-1.png';
+        console.log('pics ', pics);
+        if(staff.fields.Photo) {
+          pics = staff.fields.Photo[0].url
+        }
+        return(
+        <ul
+          key = {getImage['filename']}
+          className = 'list'
+          onClick = {props.selectedStaffMembers}>
+          <div className = 'image-contain'>
+            <img 
+              src = {pics}
+              alt = 'Staff Photos'/>
+          </div>
+                 {console.log('test key ', getImage['filename'])}
+            {/* <li className = 'staff'>{filteredOptInList}</li> */}
+            <li className = 'job'>{staff.fields.Title}</li>
+            <li className = 'number'>{staff.fields.Phone}</li>
+            <li className = 'email'>{staff.fields['Email address']}</li>
+          </ul>
+      )})}
+    </div>
     )
   }
-          
   export default DirectoryList;
+            
+
+  
+
+          
+       
+        
+          
           
         
            
