cfe8c55c5829096c9d7bba05a27b953ff428c986
Nicholas Santiago <santiago.nicholas.a@gmail.com>
2020-04-03 22:01:55 -0400

    filter working

--- src/containers/Directory/index.js
+++ src/containers/Directory/index.js
@@ -17,28 +17,32 @@ class Directory extends React.Component {
     console.log('look at ', id)                                                      
     let selectStaffMember = allStaffMembers.find(x => x.id === id);
     console.log('selectStaffMember ', selectStaffMember)                                      
-    this.setState({                                                                 selectStaffMember                                                    
-    })                                                                } 
+    this.setState({                                                                
+       selectStaffMember                                                    
+    })                                                                
+  } 
     
-  filterTheWholeDirectory = text => {
-    const { crewDirectory } = this.state;
-    const showResults = crewDirectory.filter(search => {
-      const grabData = (
-        search.Name +
-        search.Phone +
-        search.id
-      )
-      .replace (/[^a-zA-Z0-9]/g, "")     // need to read up more on regex
-      .toLowerCase();
+  // filterTheWholeDirectory = text => {
+  //   const { crewDirectory } = this.state;
+  //   const showResults = crewDirectory.filter(search => {
+  //     const grabData = (
+  //       search.Name +
+  //       search.Phone +
+  //       search.id +
+  //       search.Email +
+  //       search.Directory
+  //     )
+  //     .replace (/[^a-zA-Z0-9]/g, "")     // need to read up more on regex
+  //     .toLowerCase();
 
-      const textFormatted = text.toLowerCase()
-      return grabData.indexOf(textFormatted.replace(/\s/g, "")) > -1;   // i dont understand the regex.
-    })
-    this.setState({
-      directorySearch: showResults,
-      text
-    });
-  };
+  //     const textFormatted = text.toLowerCase()
+  //     return grabData.indexOf(textFormatted.replace(/\s/g, "")) > -1;   // i don't  understand the regex.
+  //   })
+  //   this.setState({
+  //     directorySearch: showResults,
+  //     text
+  //   });
+  // };
 
   handleInput = e => {
     console.log(e.target.value);
@@ -46,6 +50,19 @@ class Directory extends React.Component {
       searchDirectory: e.target.value
     })
   };
+
+ 
+
+  // filterWhatIsPicked = () => {
+  //   let filterDirectory = this.state.crewDirectory.filter(search => {
+  //     let grabData = (
+  //       search.Name +
+  //       search.Phone +
+  //       search['Email Address']
+  //     )
+  //     return grabData.includes(this.state.searchDirectory)
+  //   })
+  // }
 
 // filterList = text => {
 //   const {crewDirectory} = this.state;
@@ -64,15 +81,43 @@ class Directory extends React.Component {
         console.log("directory data ", responseData);
         const crewDirectory = responseData.records;
         console.log("crewDirectory ", crewDirectory);
-        this.setState({crewDirectory: crewDirectory})
+        this.setState({
+          crewDirectory: crewDirectory, 
+          allDirectory: crewDirectory,
+        })
+
       });
     }
 
+  filterDirectory = searchTerm => {
+    console.log('searchTerm ', searchTerm)
+    let directory = this.state.allDirectory;
+    const formattedSearch = searchTerm.toLowerCase();
+    console.log(directory)
+    
+    const results = directory.filter(person => {
+      console.log(person)
+      let grabData = (
+        person.fields.Name +
+        person.fields.Phone +
+        person.fields['Email Address']
+      )
+      .replace(/[^a-zA-Z0-9]/g, "")
+      .toLowerCase();
+      return grabData.indexOf(formattedSearch) > -1;
+      
+      
+    })
+    console.log(results)
+    this.setState({ crewDirectory: results })
+    
+      // console.log('grabData ', grabData === grabData)
+  
+    }
   render() {
-    // let filterDirectory = this.state.crewDirectory.filter(search => {
-    //   return search.fields.Name.toUpperCase().includes(this.state.searchDirectory.toUpperCase()
-    //   );
-    // })
+    
+    
+    
 
    
     // let filterNumber = this.state.crewDirectory.filter(phone => {
@@ -85,14 +130,16 @@ class Directory extends React.Component {
           crewDirectory={this.state.crewDirectory}
           selectedStaffMember={this.selectedStaffMember}
           handleInput={this.handleInput}
-          filterTheWholeDirectory={this.filterTheWholeDirectory}
+          filterDirectory={this.filterDirectory}
+
+          // filterTheWholeDirectory={this.filterTheWholeDirectory}
         />
         )}
         {this.state.crewDirectory && (<DirectoryList 
           crewDirectory={this.state.crewDirectory}
           selectedStaffMember={this.selectedStaffMember}
-          filterTheWholeDirectory={this.filterTheWholeDirectory}
-          // filterDirectory={filterDirectory}
+          // filterTheWholeDirectory={this.filterTheWholeDirectory}
+          // filterDirectory={this.filterDirectory}
           // filterNumbers={filterNumber}                                                 // Part of the phone number filter.
         />)}
      </div>
