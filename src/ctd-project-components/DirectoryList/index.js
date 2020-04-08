ba1d5984bed3ef504786640cf5edb8b56a69b547
Nicholas Santiago <santiago.nicholas.a@gmail.com>
2020-04-08 10:13:34 -0400

    it only spins the whole page

--- src/ctd-project-components/DirectoryList/index.js
+++ src/ctd-project-components/DirectoryList/index.js
@@ -1,58 +1,69 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import ReactCardFlip from 'react-card-flip';
 
 const DirectoryList = (props) => {
   // all ctd staff info
   let staffList = props.crewDirectory;
   console.log('staffList ', staffList);
+
   // opt in members list
   let optInStaffMembers = staffList.filter(filterList  => 
     filterList.fields.Directory === "YES, Include my contact info in CTD Team Directory")
     console.log("optInStaffMembers ", optInStaffMembers);
   let getImage = optInStaffMembers.map(( renderImage ) => renderImage.fields.Photo);
   console.log('getImage ', getImage)
-  // let searchByNum = props.filterNumber;                                                // trying to figure out how to filter by phone numbers
-  // let newOptInList = optInStaffMembers.concat(searchByNum);
-  // console.log('newOptInList ', newOptInList)
-  // console.log('hello world')
-  // let searchByNameAndNumber = optInStaffMembers
-  // console.log("newSearch ", searchByNameAndNumber)
-  // searchByNameAndNumber.filter(search => {
-  //   let grabData = (search.Name + search.Phone + search['Email Address'])
-  //   console.log('search ', search);
-    // console.log('grabData ', grabData)
-    
-  // })
+
+  // begin code for card flip
+  const [isFlipped, setIsFlipped] = useState(false);
+  const handleClick = () => {
+    setIsFlipped(!isFlipped)
+  }
   return(
-    <div className = 'list-container'>
-      {optInStaffMembers.map(staff => {
-        let pics = 'https://ya-webdesign.com/images250_/placeholder-image-png-1.png';
-        {/* console.log('pics ', pics); */}
-        if(staff.fields.Photo) {
-          pics = staff.fields.Photo[0].url
-        }
-        return(
-        <ul
-          className = 'list'
-          onClick = {() => props.selectedStaffMember(staff.id)}
-          key = {staff.fields.Names}>
-          {/* {console.log(staff.fields.Name)} */}
-          <div className = 'image-contain'>
-            <img 
-              src = {pics}
-              alt = 'Staff Photos'/>
-          </div>
-              {/* {console.log('test key ', getImage['filename'])} */}
-            <li className = 'staff'>{staff.fields.Name.toUpperCase()}</li>
-            <li className = 'job'>{staff.fields.Title}</li>
-            <li className = 'number'>{staff.fields.Phone}</li>
-            <li className = 'email'>{staff.fields['Email address']}</li>
-          </ul>
-      )})}
-    </div>
+    <ReactCardFlip isFlipped = {isFlipped} flipDirection = 'horizontal'>
+      <div className = 'list-container'>
+        {optInStaffMembers.map(staff => {
+          let pics = 'https://ya-webdesign.com/images250_/placeholder-image-png-1.png';
+          if(staff.fields.Photo) {
+            pics = staff.fields.Photo[0].url
+          }
+          return(
+            <ul
+              className = 'list'
+              onClick = {handleClick}
+              key = {staff.fields.Names}>
+              <div className = 'image-contain'>
+                <img 
+                  src = {pics}
+                  alt = 'Staff Photos'/>
+              </div>
+                <li className = 'staff'>{staff.fields.Name.toUpperCase()}</li>
+                <li className = 'job'>{staff.fields.Title}</li>
+                <li className = 'number'>{staff.fields['Primary Department']}</li>
+                {/* <li className = 'email'>{staff.fields['Email address']}</li> */}
+            </ul> 
+        )})}
+      </div>
+      <div className = 'back-of-list'>
+            {optInStaffMembers.map(back => {
+              console.log('hello world')
+              const backInfo = back.fields;
+              console.log('backInfo ', backInfo)
+              return(
+                <ul className = 'back-of-list'
+                    onClick = {handleClick}>
+                      <li>{back.fields.Phone}</li>
+                    </ul>
+              )
+            })}
+      </div>
+    </ReactCardFlip>
     )
   }
   export default DirectoryList;
+
+
+
+
             
 
   
