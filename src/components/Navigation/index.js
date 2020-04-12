92ddf55a843816d1bfa58f2edcffe2e20d03f385
Elsabeth Kidane <elizit88@gmail.com>
2020-04-11 20:13:06 -0400

    landing page

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,15 +3,19 @@ import { Link } from "react-router-dom";
 import * as ROUTES from "../../constants/routes.js";
 import Logo from "../../assets/images/ctd-logo.png";
 import SignOutButton from "../SignOut";
-// import Sidebar from "../Sidebar";
-// import StaffDirectory from "../../ctd-project-components/StaffDirectory";
-
+import {Redirect} from "react-router-dom";
+import  Userprofile  from "../Userprofile";
 import { AuthUserContext } from "../Session";
 
 const Navigation = () => (
   <AuthUserContext.Consumer>
     {authUser =>
-      authUser ? <NavigationAuth authUser={authUser} /> : <NavigationNonAuth />
+            authUser ? <NavigationAuth authUser={authUser} /> :
+                <div>
+                    
+                    <NavigationNonAuth />
+                <Redirect to ="/signin"/>
+                    </div>
     }
   </AuthUserContext.Consumer>
 );
@@ -24,21 +28,23 @@ const NavigationAuth = () => (
       <span className="nav-icon"></span>
     </label>
     <ul className="menu">
-      <li>
-        <Link to={ROUTES.HOME}>Home</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.PROJECT}>Projects</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.DIRECTORY}>Directory</Link>
-      </li>
-      <li>
-        {" "}
-        <Link to={ROUTES.ACCOUNT}>Account</Link>
-      </li>
-      <li>
-        <SignOutButton />
+            {/* <li>
+        <Link to={ROUTES.ARTICLES}>Articles</Link>
+      </li>*/}
+            {/* <li>
+        <Link to={ROUTES.PROJECT}>project</Link>
+      </li>*/}
+                {/* <li>
+      
+                <Link to={ROUTES.ACCOUNT}>Account</Link> 
+                
+      </li>*/}
+                < li >
+                <Userprofile />
+               
+                {/* <i className="fa fa-user fa-xs" />{" "}<i className="fa fa-caret-down">  */}
+                    
+          
       </li>
     </ul>
   </header>
@@ -63,7 +69,8 @@ const NavigationNonAuth = () => (
       <li>
         {" "}
         <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
+            </li>
+          
     </ul>
   </header>
 );
