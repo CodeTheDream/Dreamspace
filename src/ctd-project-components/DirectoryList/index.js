310f5d04b0a701bdf47fb421f65482d3dcf00aa6
Nicholas Santiago <santiago.nicholas.a@gmail.com>
2020-04-15 20:34:22 -0400

    sorted list and flipped cards

--- src/ctd-project-components/DirectoryList/index.js
+++ src/ctd-project-components/DirectoryList/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import ReactCardFlip from 'react-card-flip';
+import ReactCardFlip from 'react-card-flip';
 
 const DirectoryList = (props) => {
   // all ctd staff info
@@ -10,30 +10,74 @@ const DirectoryList = (props) => {
   let optInStaffMembers = staffList.filter(filterList  => 
     filterList.fields.Directory === "YES, Include my contact info in CTD Team Directory")
     console.log("optInStaffMembers ", optInStaffMembers);
+    let sortNames = optInStaffMembers;
+    console.log('let\'s see the names', sortNames)
+    sortNames.sort((a, b) => (a.fields['First Name'] > a.fields['Last Name']) ? 1 : -1);
+
+    // begin code for card flip
+    const [isFlipped, setIsFlipped] = useState(false);
+    const handleClick = () => {
+      setIsFlipped(!isFlipped)
+    }
+    return(
+      <div className = 'list-container'>
+          {sortNames.map(staff => {
+            let pics = 'https://ya-webdesign.com/images250_/placeholder-image-png-1.png';
+            if(staff.fields.Photo) {
+              pics = staff.fields.Photo[0].url
+            }
+            
+            return(
+             <ReactCardFlip isFlipped = {isFlipped} flipDirection = 'horizontal'>  
+            
+  
+              <ul
+                className = 'list'
+                onClick = {handleClick}
+                key = {staff.fields}>
+                <div className = 'image-contain'>
+                  <img 
+                    src = {pics}
+                    alt = 'Staff Photos'/>
+                </div>
+                  <li className = 'staff'>{staff.fields.Name}</li>
+                  <li className = 'location'>{staff.fields.Location}</li>
+                  <li className = 'job'>{staff.fields.Title}</li>
+                  <li className = 'primary-job'>{staff.fields['Primary Department']}</li>
+                  {/* <li className = 'email'>{staff.fields['Email address']}</li> */} 
+              </ul> 
+  
+              <ul className = 'back-of-list'
+                  onClick ={handleClick}>
+                  <div className = 'rear-contain'
+                       style = {{paddingTop: '234px'}}>
+
+
+                  </div>
+                <li className = 'slack'>Slack Name: {staff.fields['Slack Name']}</li>
+                <li className = 'number'>{staff.fields.Phone}</li>
+                <li className = 'email'>{staff.fields['Email address']}</li>
+              </ul>
+            
+             </ReactCardFlip> 
+          )})}
+        </div>
+     )
+    }
+  
+    export default DirectoryList;
+     
+      
+    
+
+
+          
+            
 
   
-    let aToZOrder = optInStaffMembers.sort((a, b) => {
-      let firstName = a.fields['First Name'].toUpperCase();
-      let lastName = b.fields['Last Name'].toUpperCase();
-      console.log('check first ', firstName);
-      console.log('check for last ', lastName);
-      return a.name > b.name ? 1 : -1;
-    })
-    console.log('final results of the order ', aToZOrder);
-    
+
  
 
-  // begin code for card flip
-  // const [isFlipped, setIsFlipped] = useState(false);
-  // const handleClick = () => {
-  //   setIsFlipped(!isFlipped)
-  // }
-  return(
-    <div className = 'list-container'>
-        {aToZOrder.map(staff => {
-        
-          
-          let pics = 'https://ya-webdesign.com/images250_/placeholder-image-png-1.png';
           
         
 
@@ -41,41 +85,6 @@ const DirectoryList = (props) => {
 
          
           
-          if(staff.fields.Photo) {
-            pics = staff.fields.Photo[0].url
-          }
-          return(
-          // <ReactCardFlip isFlipped = {isFlipped} flipDirection = 'horizontal'>  
-          // </ReactCardFlip> 
-          <div className = 'check'>
-
-            <ul
-              className = 'list'
-              // onClick = {handleClick}
-              key = {staff.fields.Names}>
-              <div className = 'image-contain'>
-                <img 
-                  src = {pics}
-                  alt = 'Staff Photos'/>
-              </div>
-                <li className = 'staff'>{staff.fields.Name.toUpperCase()}</li>
-                <li className = 'location'>{staff.fields.Location}</li>
-                <li className = 'job'>{staff.fields.Title}</li>
-                <li className = 'primary-job'>{staff.fields['Primary Department']}</li>
-                {/* <li className = 'email'>{staff.fields['Email address']}</li> */} 
-            </ul> 
-
-            <ul className = 'back-of-list'>
-              <li className = 'slack'>Slack Name: {staff.fields['Slack Name']}</li>
-              <li className = 'number'>{staff.fields.Phone}</li>
-              <li className = 'email'>{staff.fields['Email address']}</li>
-            </ul>
-          </div>
-        )})}
-      </div>
-   )
-  }
-  export default DirectoryList;
 
    
 
