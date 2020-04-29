ee62d3d7e3e8ccfbda35b71a069122d3b9980990
Elsabeth Kidane <elizit88@gmail.com>
2020-04-28 21:19:40 -0400

    rebasing Nick's page

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -12,17 +12,17 @@ const HomePage = () => {
 
             </div>
             <div className="card1">
-                <p><button> <Link to="/articles" style={{ color: "white" ,fontSize:"24px" }}>Projects </Link></button></p>
+                <p><button> <Link to="/projects" style={{ color: "white" ,fontSize:"24px" }}>Projects </Link></button></p>
 
 
                 <Link to="/projects"><img src="https://stackify.com/wp-content/uploads/2013/08/best-dev-websites-2-793x397.jpg" alt="projects" className="myprofile" /></Link>
 
             </div>
             <div className="card1">
-                <p><button> <Link to="/articles" style={{ color: "white", fontSize: "24px" }}>CompanyDirectory</Link></button></p>
+                <p><button> <Link to="/directory" style={{ color: "white", fontSize: "24px" }}>CompanyDirectory</Link></button></p>
 
 
-                <Link to="/projects"><img src="https://res.cloudinary.com/wnotw/images/c_limit,w_1536,q_auto:eco,f_auto/v1570469574/l6b1dynqevwroklkifec/code-the-dream" alt="cdirectory"className="myprofile" /></Link>
+                <Link to="/directory"><img src="https://res.cloudinary.com/wnotw/images/c_limit,w_1536,q_auto:eco,f_auto/v1570469574/l6b1dynqevwroklkifec/code-the-dream" alt="cdirectory"className="myprofile" /></Link>
 
             </div>
 
