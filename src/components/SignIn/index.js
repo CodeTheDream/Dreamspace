01ecc054c7bed7a418fa96bb1acef7b66fd80d56
Elsabeth Kidane <elizit88@gmail.com>
2020-06-10 12:41:08 -0400

    my update

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -134,6 +134,7 @@ class SignInGoogleBase extends Component {
                      {
                        username: socialAuthUser.user.displayName,
                        email: socialAuthUser.user.email,
+                       photoUrl: socialAuthUser.user.photoURL,
                        roles: {},
                      },
                      { merge: true },
@@ -143,6 +144,7 @@ class SignInGoogleBase extends Component {
                    this.setState({ error: null });
                    this.props.history.push(ROUTES.HOME);
                  })
+
                  .catch(error => {
                    if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
                      error.message = ERROR_MSG_ACCOUNT_EXISTS;
