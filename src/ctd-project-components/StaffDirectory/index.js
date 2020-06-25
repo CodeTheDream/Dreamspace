1514f330bb48190efdae9093c0a33d8f362a7068
aster <rtesagg84@gmail.com>
2020-06-25 19:03:13 -0400

    resolve conflict

---
+++ src/ctd-project-components/StaffDirectory/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StaffDirectory = (props) => {
+  let getStaffInfo = "nick"
+  console.log('getStaffInfo', getStaffInfo)
+  return(
+    <></>
+  )
+}
+
+export default StaffDirectory 
