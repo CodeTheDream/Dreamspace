af9c21264aed78695e9fd49e7c62daf6be36447c
aster <rtesagg84@gmail.com>
2020-06-25 19:02:04 -0400

    resolve conflict

--- src/ctd-project-components/AdminSignInPage/index.js
+++ src/ctd-project-components/AdminSignInPage/index.js
@@ -1,85 +1,26 @@
 import React from 'react';
-import { SignUpLink } from '../../components/SignUp'
 import { withFirebase } from "../Firebase";
 import { withRouter } from "react-router-dom";
 import { compose } from "recompose";
 import * as ROUTES from "../../constants/routes";
 
-
-const INITIAL_STATE = {
-  email: '',
-  password: '',
-  error: null,
-};
-
-class AdminAuthSignIn extends React.Component {
-  state={
-    ...INITIAL_STATE
-  };
-
-  onSubmit = (event) => {
-    const {
-      email, 
-      password
-    } = this.state;
-
-    this.props.firebase
-    .doSignInWithEmailAndPassword(email, password)
-    .then(() => {
-      this.setState({...INITIAL_STATE });
-      this.props.history.push(ROUTES.PROJECT);
-    });
-    .catch(error => {
-      this.setState({ error });
-    });
-
-    event.preventDefault();
-  };
-
-  onChange = (event) => {
-    const { email, password, error } = this.state;
-    const isInvalid = password === '' || email === '';
-  }
-  render() {
-    return(
-      <AdminSignInPage />
-    )
-  }
-}
-
 const AdminSignInPage = (props) => {
+  let firstAdminUser = props.firebase;
+  console.log('firstAdminUser ', firstAdminUser);
   return(
     <div className = 'admin-signIn-container'>
-      <form onSubmit = {this.onSubmit}>
+      <form>
         <label>
           Enter email:
-          <input
-            name = 'email'
-            value = { email }
-            onChange = { this.onChange }
-            type = 'text'
-            placeholder = 'Email Address'
-          />
-          <input
-            name = 'password'
-            value = { password }
-            onChange = { this.onChange }
-            type = 'password'
-            placeholder = 'Password'
-          />
+          <input type = 'text' value = 'value'/>
+          Enter password:
+          <input type = 'password' />
         </label>
           <input type = 'submit' value = 'Submit'/>
       </form>
     </div>
   )
 }
-
-const AdminSignInForm = compose(
-  withRouter,
-  withFirebase,
-)(AdminAuthSignIn);
 export default AdminSignInPage;
-
-export { AdminSignInForm }
 
 
