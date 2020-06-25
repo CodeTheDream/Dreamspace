669a1e9642d46bb3790eacae0c0b480e92d2cf8f
aster <rtesagg84@gmail.com>
2020-06-25 19:00:56 -0400

    resolve conflict

--- src/ctd-project-components/DayOfWeek/index.js
+++ src/ctd-project-components/DayOfWeek/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
 
 class DayOfWeek extends React.Component {
+  state = {
+    date: new Date()
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(()=>
+    this.tick(),
+    1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
   render() {
     let date = new Date();
     let weekday = new Array(7);
@@ -13,9 +33,11 @@ class DayOfWeek extends React.Component {
     weekday[6] = "Saturday";
 
     let getTheDayOfTheWeek = weekday[date.getDay()];
+    let getCurrentTime = this.state.date.toLocaleTimeString();
     return(
       <div className = 'today'>
         <p className = 'style-day'>{getTheDayOfTheWeek}</p>
+        <p className = 'style-time'>{getCurrentTime}</p>
       </div>
     )
   }
