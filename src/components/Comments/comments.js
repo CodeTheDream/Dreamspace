a3fe6c12c9fab32fbecec8710010ce7702464d70
aster <rtesagg84@gmail.com>
2020-06-26 00:51:44 -0400

    resolve conflict

--- src/components/Comments/comments.js
+++ src/components/Comments/comments.js
@@ -39,8 +39,24 @@
 //     });
 //   };
 
+<<<<<<< HEAD
 //   handleSubmit = (e, authUser) => {
 //     e.preventDefault();
+=======
+  getSingleComment = (id) => {
+    this.props.firebase.comment(id)
+    .onSnapshot(snapshot => {
+      console.log('getting comment', snapshot.id)
+      
+
+      // console.log("Articles loaded here yo!", articles);
+      
+    });
+  }
+
+  handleSubmit = (e, authUser) => {
+    e.preventDefault();
+>>>>>>> new update
 
 //     this.props.onCreate(this.state);
 //   // this. props.refreshFunction(this.state)
