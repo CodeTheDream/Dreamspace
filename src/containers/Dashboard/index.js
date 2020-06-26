c1a5b2ddf98c37b10d186957efc54f600b7e8000
aster <rtesagg84@gmail.com>
2020-06-26 01:46:13 -0400

    resolve conflict

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -55,8 +55,8 @@ class Dashboard extends React.Component {
 
     let filteredArticles = this.state.articles.filter(article => {
       return (
-          article.tags.toLowerCase().includes(this.state.search.toLowerCase()),
-         // article.timeCreated.toLowerCase().includes(this.state.search.toLowerCase()),
+         // article.tags.toLowerCase().includes(this.state.search.toLowerCase()),
+          article.timeCreated.toLowerCase().includes(this.state.search.toLowerCase()),
         article.title.toLowerCase().includes(this.state.search.toLowerCase())
        // article.description
          // .toLowerCase()
--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import directory from "../assets/images/Directorypage.gif"
+//import directory from "./assets/images/Directorypage.gif"
+import directory from '../../assets/images/directorypage1.gif';
+import article from '../../assets/images/articles.gif';
 const HomePage = () => {
     return (
         <div className="wrapper1">
             <div className="card1">
                 <p><button> <Link to="/articles" style={{ color: "white", fontSize: "24px" }}>Articles </Link></button></p>
 
-
-                <Link to="/articles"><img src="https://images.idgesg.net/images/article/2017/06/reactjs_code_coding_thinkstock-100725807-large.jpg" alt="articles"className="myprofile" /></Link>
+                <Link to="/articles"><img src={article} alt="article" className="myprofile" /></Link>
+                {/*<Link to="/articles"><img src="https://images.idgesg.net/images/article/2017/06/reactjs_code_coding_thinkstock-100725807-large.jpg" alt="articles"className="myprofile" /></Link>*/}
 
             </div>
             <div className="card1">
