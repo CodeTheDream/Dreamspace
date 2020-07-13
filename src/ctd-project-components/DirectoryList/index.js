8c91c64db3102bbfab8d99f294a89a1ae25b0450
Nicholas Santiago <santiago.nicholas.a@gmail.com>
2020-07-13 17:04:41 -0400

    langueage type images on directory

--- src/ctd-project-components/DirectoryList/index.js
+++ src/ctd-project-components/DirectoryList/index.js
@@ -3,6 +3,30 @@ import ReactCardFlip from 'react-card-flip';
 import reactImage from '../../assets/images/react-image.png';
 import rubyImage from '../../assets/images/ruby-on-rails.png';
 
+const secondaryImage = (staff) => {
+  console.log('pppp', staff);
+  if(checkDepartment(staff.fields['Primary Department']) === 'react') {
+  
+  return (<img className = 'react-pic' src = {reactImage} alt = 'react' />)
+  }
+
+if(checkDepartment(staff.fields['Primary Department']) === 'ruby') {
+  return (<img src = {rubyImage} alt = 'ruby' />); 
+  } 
+}
+const checkDepartment = (departments) => {
+  console.log('dddddd', departments);
+  for(let i = 0; i < departments.length; i ++) {
+    if(departments[i] === 'Front End (React JS)') {
+      return 'react';
+    }
+    if(departments[i] === 'Back End (Ruby on Rails)') {
+      return 'ruby';
+    }
+  }
+}
+
+ 
 const DirectoryList = (props) => {
   // all ctd projects
   let projects = props.projectData;
@@ -21,6 +45,11 @@ const DirectoryList = (props) => {
     let sortNames = optInStaffMembers;
     console.log('let\'s see the names', sortNames)
     sortNames.sort((a, b) => (a.fields['First Name'] > b.fields['First Name']) ? 1 : -1);
+    sortNames.map(job => {
+      console.log('hehehehehhehehehhehe', job.fields['Primary Department']);
+      let department = job.fields['Primary Department'];
+      console.log('check this out ', department);
+    });
 
   // Randomize the flip direction on the cards
     const getFlip = ['vertical', 'horizontal'];
@@ -34,7 +63,6 @@ const DirectoryList = (props) => {
             if(staff.fields.Photo) {
               pics = staff.fields.Photo[0].url
             }
-            
             return(
              <ReactCardFlip key = {staff.id} isFlipped = {staff.id === props.isFlipped} flipDirection = {randomFlip}>  
               <ul
@@ -54,19 +82,15 @@ const DirectoryList = (props) => {
   
               <ul className = 'back-of-list'
                   onClick = {() => props.selectedStaffMember(null)}>
-                  <div className = 'back-card-directory-image' style = {{
+                  {/* <div className = 'back-card-directory-image' style = {{
                     width: '100px', height: '100px', borderRadius: '50%'}}>
                     <img src = {pics} alt = 'Staff Member' style = {{
                       width: '100px', height: '100px', borderRadius: '50%', marginLeft: '10px', marginTop: '10px'}}/>
+                  </div> */}
+                  <div>
+                      {secondaryImage(staff)}
+                    {/* <img className = 'react-pic' src = {reactImage} alt = 'react' /> */}
                   </div>
-                    <div className = 'frontend-or-backend'>
-                      {}
-                    <div>
-                    
-                  </div>
-                    {/* <div>{staff.fields['Primary Department'][0] === 'reactjs' : <img src = {rubyImage} alt = 'second-logo'/>}</div> */}
-                  </div>
-                  {/* <h4 className = 'project' style = {{textAlign: 'center', fontWeight: 'bold'}}>Current Project</h4> */}
                   <li className = 'slack' style = {{marginTop: '10px'}}><strong>Slack Name:</strong> {staff.fields['Slack Name']}</li>
                   <li className = 'number'><strong>Phone Number:</strong> {staff.fields.Phone}</li>
                   <li className = 'email'><strong>Email:</strong> {staff.fields['Email address']}</li>
