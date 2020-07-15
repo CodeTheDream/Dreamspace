e32cd40655f55f1f1a406c3e29f3a37a8d888655
aster <rtesagg84@gmail.com>
2020-07-15 10:42:48 -0400

    new update

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -76,7 +76,11 @@ class Dashboard extends React.Component {
       });
     }
     return (
+<<<<<<< HEAD
       <div className="dashboard_wrapper">
+=======
+      <div className="dashbord_wrapper">
+>>>>>>> new update
         <div className="main-class">
           <div className="search-bar">
             <SearchBar handleInput={this.handleInput} />
