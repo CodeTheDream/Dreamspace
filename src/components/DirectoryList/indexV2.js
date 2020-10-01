8094d10413caef85811f1dc2d1ac48d5b8180eac
Daniel Carbajal <carbajal869@gmail.com>
2020-10-01 15:52:44 -0400

    Fetch and display info from airtable in IndexV2

--- src/components/DirectoryList/indexV2.js
+++ src/components/DirectoryList/indexV2.js
@@ -10,19 +10,31 @@ const DirectoryList = (props) => {
     //const firstName = props.crew.map(name => console.log(name.fields['First Name']));
     return (
         <div>
+            
             <form className="search-cards">
                 <input className="search-bar" type="text" placeholder="Search.." name="search"></input>
                 <input type="image" className="binoculars" id="image" alt="search" src={Binoculars}></input>
             </form>
             <div className="individual-cards">
-                {/*{props.crew.map(name => <p>{name.fields['First Name']}</p>)}*/}
+                {props.crew.map(name => {
+                    console.log(name)
+                    return(
+                        <>
+                        <img src={name.fields.Photo}/>
+                        <p>{name.fields.Name}</p>
+                        <p>{name.fields.Title}</p>
+                        <p>{name.fields['Primary Department']}</p>
+                        <p>{name.fields.Location}</p>
+                        </>
+                    )
+                })}
             </div>
-            <div className="last-names">
+            {/*<div className="last-names">
                 {props.crew.map(name => <p className="full-names">{name.fields.Name}</p>)}
             </div>
             <div className="roles">
                 {props.crew.map(roles => <p className="job-roles">{roles.fields['Email address']}</p>)}
-            </div>
+    </div>*/}
 
 
 
