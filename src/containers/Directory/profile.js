5353acbe0efdc2b14ea4f52cad3d7129736ac196
scbonner <45452011+scbonner@users.noreply.github.com>
2021-01-21 04:38:30 -0500

    making profile sheet functional

--- src/containers/Directory/profile.js
+++ src/containers/Directory/profile.js
@@ -1,9 +1,9 @@
-import React, { useState, } from "react"
+import React, { Component } from "react"
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
+import * as ROUTES from "../../constants/routes";
+import * as ROLES from "../../constants/roles";
 import styled from 'styled-components';
-
-
 
 
 
@@ -46,6 +46,7 @@ const FormWrapper = styled.div`
     flexWrap: wrap; 
     alignItems: center; 
     justifyContent: flex-start; 
+
 } 
 `
 const span = styled.div`
@@ -74,7 +75,12 @@ const i = styled.div`
   }
 `
 const inputwrapper = styled.div`
+    width: 100;
+    display: flex;
     flexGrow: 1; 
+    flexFlow: nowrap;
+    alignItems: stretch;
+    justifyContent: center;
     minHeight: 2rem; 
     padding: 0.375rem 0.75rem;                         
     display: block; 
@@ -87,12 +93,11 @@ const inputwrapper = styled.div`
     fontWeight: 100; 
     lineHeight: 1.75; 
     color: #495057;
+
+  
   }
 `; 
-const hr = styled.div`
-  border-color: black;
-  }
-`;
+
 
 
 
@@ -113,10 +118,140 @@ const hr = styled.div`
 //     </div>
 //   );
 
+ const INITIAL_STATE = {
+  
+  title: "",
+  email: "",
+  projects: "",
+  language: "",
+  mentor: "",
+  state: "",
+  country: "",
+  image: "",
+  photo: "",
+  remote: "",
+  equipment: "",
+  passwordOne: "",
+  passwordTwo: "",
+  isAdmin: false,
+  error: null
+ 
+};
+const ERROR_CODE_ACCOUNT_EXISTS = "auth/email-already-in-use";
+const ERROR_MSG_ACCOUNT_EXISTS = `
+ 
+//   An account with this email address already exists. 
+ 
+//   Please sign in instead. 
+ 
+// `;
 
-function Profile() {
-    const [selectedPerson, setSelectedPerson] = useState(null);
-      return(
+
+
+class Profile extends Component {
+ constructor(props) {
+   super(props);
+   this.state = { ...INITIAL_STATE };
+ }
+ onSubmit = event => {
+   const { 
+  title,
+  email,
+   projects,
+   language,
+   mentor,
+   state,
+   country,
+   image,
+   photo,
+   remote,
+   equipment,
+   passwordOne,
+   passwordTwo,
+   isAdmin,
+   error,
+  
+  } = this.state;
+  const roles = {};
+   if (isAdmin) {
+     roles[ROLES.ADMIN] = ROLES.ADMIN;
+   }
+   // else {
+   //   roles[ROLES.NONADMIN] = ROLES.NONADMIN;
+   // }
+   console.log("roles: ", roles);
+   this.props.firebase
+     .doCreateUserWithEmailAndPassword(email, passwordOne)
+     .then(authUser => {
+       // Create a user in your Firebase realtime database
+       return this.props.firebase.user(authUser.user.uid).set(
+         {
+          title,
+          email,
+          projects,
+          language,
+          mentor,
+          state,
+          country,
+          image,
+          photo,
+          remote,
+          equipment,
+          passwordOne,
+          passwordTwo,
+          isAdmin,
+          error,
+         },
+         { merge: true }
+       );
+     })
+     .then(() => {
+       return this.props.firebase.doSendEmailVerification();
+     })
+     .then(() => {
+       this.setState({ ...INITIAL_STATE });
+       this.props.history.push(ROUTES.HOME);
+     })
+     .catch(error => {
+       if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+         error.message = ERROR_MSG_ACCOUNT_EXISTS;
+       }
+       this.setState({ error });
+     });
+   event.preventDefault();
+ };
+ onChange = event => {
+   this.setState({ [event.target.name]: event.target.value });
+ };
+ onChangeCheckbox = event => {
+   this.setState({ [event.target.name]: event.target.checked });
+ };
+ render() {
+   const {
+    title,
+    email,
+    projects,
+    language,
+    mentor,
+    state,
+    country,
+    image,
+    photo,
+    remote,
+    equipment,
+    passwordOne,
+    passwordTwo,
+    isAdmin,
+    error
+
+   } = this.state;
+   const isInvalid =
+     passwordOne !== passwordTwo ||
+     passwordOne === "" ||
+     email === "" ||
+     title === "";
+     
+    return(
       <main style=
       {{ minHeight: `100vh`, padding: `3rem 3rem`, }} classNumber="has-dflex-center">
         <section style=
@@ -156,16 +291,226 @@ function Profile() {
            </div>
            </div> 
 
+          <FormWrapper>
+          <form action="get">
+            <div className="fieldset">
+              <div className="input-wrapper"> 
+                <span  className="icon"> 
+                  <i className="fas fa-network-wired fa-1x"/></span>
+                  &nbsp;&nbsp;
+                  &nbsp;&nbsp;
+                  <input list="title" id="title-choice" name="title-choice" placeholder="Title" />
+                    <datalist id="title">
+                        <option value="Staff" />
+                        <option value="Intern" />
+                        <option value="Volunteer" />
+                     </datalist>
+              </div>
+            </div>  
+
+            <div className="fieldset">
+              <div className="input-wrapper"> 
+                <span  className="icon"> 
+                  <i className="fab fa-dev fa-1x"/></span>
+                  &nbsp;&nbsp;
+                  &nbsp;&nbsp;
+                <input list="dev" id="dev-choice" name="dev-choice" placeholder='Developer' required/>
+                    <datalist id="dev">
+                        <option value="Frontend" />
+                        <option value="Backend" />
+                        <option value="Full Stack" />
+                     </datalist>
+              </div>
+            </div> 
+
+            <div className="fieldset">
+              <div className="input-wrapper"> 
+                <span className="icon"> 
+                  <i className="fab fa-buffer fa-1x"/></span>
+                  &nbsp;&nbsp;
+                  &nbsp;&nbsp;
+                  <input list="projects" id="projects-choice" placeholder="Projects" name="projects-choice" />
+                      <datalist id="projects">
+                          <option value="AACT" />
+                          <option value="Clinwiki" />
+                          <option value="DreamSpace" />
+                          <option value="NC Fair Chance" />
+                          <option value="Vamos" />
+                          <option value="Mural AR" />
+                          <option value="Upstate" />
+                          <option value="NCMA" />
+                      </datalist>
+              </div>
+            </div>   
+
+            {/* <div className="fieldset">
+              <div className="input-wrapper"> 
+                <span className="icon"> 
+                  <i className="fab fa-github-square fa-1x"/></span>
+                  &nbsp;&nbsp;
+                  &nbsp;&nbsp;
+                  <input className="col-6 form-control" name="Github" onSubmit={this.onSubmit} type="text" value={name}
+                    placeholder="Github" id="username" autocomplete="username" required></input> 
+              </div>
+            </div>
+            <hr />
+
+            <div className="fieldset">
+              <div className="input-wrapper"> 
+                <span className="icon"> 
+                  <i className="fas fa-envelope fa-1x"/></span>
+                  &nbsp;&nbsp;
+                  &nbsp;&nbsp;
+                  <input className="col-6 form-control" name="Email" onSubmit={this.onSubmit} type="text" value={name}
+                    placeholder="Email" id="username" autocomplete="username" required></input> 
+              </div>
+            </div>
+
+            <div className="fieldset">
+              <div className="input-wrapper"> 
+                <span className="icon"> 
+                  <i className="fas fa-unlock-alt fa-1x"/></span>
+                  &nbsp;&nbsp;
+                  &nbsp;&nbsp;
+                  <input className="col-6 form-control" name="Password" onSubmit={this.onSubmit} type="text" value={name}
+                    placeholder="Password" id="username" autocomplete="username" required></input> 
+              </div>
+            </div>
+
+            <div className="fieldset">
+              <div className="input-wrapper"> 
+                <span className="icon"> 
+                  <i className="fas fa-lock-alt fa-1x"/></span>
+                  &nbsp;&nbsp;
+                  &nbsp;&nbsp;
+                  <input className="col-6 form-control" name="Confirm" onSubmit={this.onSubmit} type="text" value={name}
+                    placeholder="Confirm Password" id="username" autocomplete="username" required></input> 
+              </div>
+            </div> */}
+
+            {/* <div className="fieldset">
+              <div className="input-wrapper"> 
+                <span className="icon"> 
+                  <i class="fas fa-language fa-1x"/></span>
+                  &nbsp;&nbsp;
+                  &nbsp;&nbsp;
+                  <input className="col-6 form-control" name="languages" onSubmit={this.onSubmit} type="text" value={name}
+                    placeholder="Languages" id="username" autocomplete="username" required></input> 
+              </div>
+            </div>
+          
+            <div className="fieldset">
+              <div className="input-wrapper"> 
+                <span className="icon"> 
+                  <i class="fas fa-chalkboard-teacher fa-1x"/></span>
+                  &nbsp;&nbsp;
+                  &nbsp;&nbsp;
+                  <input list="mentor" id="mentor-choice" placeholder="Mentor" name="equipment" />
+                    <datalist id="mentor">
+                      <option value="Yes" />
+                      <option value="No" />
+                    </datalist>
+              </div>
+            </div> 
+
+            <div className="fieldset">
+              <div className="input-wrapper"> 
+                <span className="icon"> 
+                  <i class="fas fa-landmark fa-1x"/></span>
+                  &nbsp;&nbsp;
+                  &nbsp;&nbsp;
+                  <input className="col-6 form-control" name="State" onSubmit={this.onSubmit} type="text" value={name}
+                    placeholder="State" id="username" autocomplete="username" required></input> 
+              </div>
+            </div>
+
+            <div className="fieldset">
+              <div className="input-wrapper"> 
+                <span className="icon"> 
+                  <i class="fas fa-globe fa-1x"/></span>
+                  &nbsp;&nbsp;
+                  &nbsp;&nbsp;
+                  <input className="col-6 form-control" name="Country" onSubmit={this.onSubmit} type="text" value={name}
+                    placeholder="Country" id="username" autocomplete="username" required></input> 
+              </div>
+            </div> 
+
+            <div className="fieldset">
+              <div className="input-wrapper"> 
+                <span className="icon"> 
+                  <i class="fas fa-map fa-1x"/></span>
+                  &nbsp;&nbsp;
+                  &nbsp;&nbsp;
+                  <input className="col-6 form-control" name="image" onSubmit={this.onSubmit} type="text" value={name}
+                    placeholder="ImageOfHome" id="username" autocomplete="username" required></input> 
+              </div>
+            </div>  
+
+            <div className="fieldset">
+              <div className="input-wrapper"> 
+                <span className="icon"> 
+                  <i class="fas fa-camera fa-1x"/></span>
+                  &nbsp;&nbsp;
+                  &nbsp;&nbsp;
+                  <input className="col-6 form-control" name="Upload" onSubmit={this.onSubmit} type="text" value={name}
+                    placeholder="Upload" id="username" autocomplete="username" required></input> 
+              </div>
+            </div> 
+
+            <div className="fieldset">
+              <div className="input-wrapper"> 
+                <span className="icon"> 
+                  <i class="fas fa-podcast fa-1x"/></span>
+                  &nbsp;&nbsp;
+                  &nbsp;&nbsp;
+                  <input list="remote" id="remote-choie" placeholder="Remote" name="remote-choice" />
+                   <datalist id="remote">
+                      <option value="Yes" />
+                      <option value="No" />
+                    </datalist>
+              </div>
+            </div> 
+            <hr></hr>
+
+            <div className="fieldset">
+              <div className="input-wrapper"> 
+                <span className="icon"> 
+                  <i class="fas fa-laptop-code fa-1x"/></span>
+                  &nbsp;&nbsp;
+                  &nbsp;&nbsp;
+                  <input list="equipment" id="equipment-choice" placeholder="Equipment" name="equipment-choice" />
+                    <datalist id="equipment">
+                      <option value="Yes" />
+                      <option value="No" />
+                    </datalist>
+              </div> */}
+              
+            
+
+            <Button className="button-tertiary" type="submit" value="Submit">
+              Submit
+            </Button>
+{/* 
+            <Link to={'/directory/profile.js'}>
+            <Button> Profile Page </Button>
+            </Link>
+            {error && <p>{error.message}</p>} */}
+              
+        </form>
+      </FormWrapper>
+      
+   
+    {/* )
+ };
+}; */}
+{/* 
+
            <div style= {{ display: `flex`, alignItems: `flex-start`, justifyContent: `flex-end`, }} classNumber="lx-column">
               <form style= {{ gridTemplateColumns: `1fr 300px`, margin: `4rem 2rem`,
                                 padding: `10px`, textAlign: `left`, minWidth: `35rem`,
                                 maxWidth: `35rem`, marginTop: `30px`, }} action="get">
-                <div style= {{ width: `100%`, margin: `2rem 0`, position: `relative`, display: `flex`,
-                           flexWrap: `wrap`, alignItems: `center`, justifyContent: `flex-start`, }} 
-                           classNumber="fieldset">
-                 
-                    <div style= {{ width: `100%`, display: `flex`, flexFlow: `nowrap`, alignItems: `stetch`, justifyContent: `center`,
-                                }} classNumber="input-wrapper"> 
+                <div classNumber="fieldset">
+                    <div classNumber="input-wrapper"> 
                         <span style= {{ width: `fit-content`, margin: `0`, padding: `1rem 1rem`, display: `flex`, alignItems: `center`,
                                         borderTopLeftRadius: `0.25em`, borderBottomLeftRadius: `0.25em`, borderTopRightRadius: `0`,
                                         borderBottomRightRadius: `0`, border: `0.0625rem solid #ced4da`, fontSize: `1rem`,
@@ -228,7 +573,7 @@ function Profile() {
                            classNumber="fieldset">
                 {/* <label style= {{ width: `100%`, margin: `0 0 1rem 1`, fontSize: `1.2rem`, fontWeight: `700`, }} for="user-name"> Email:
                 </label> */}
-                    <div style= {{ width: `100%`, display: `flex`, flexFlow: `nowrap`, alignItems: `stetch`, justifyContent: `center`,
+                    {/* <div style= {{ width: `100%`, display: `flex`, flexFlow: `nowrap`, alignItems: `stetch`, justifyContent: `center`,
                                 }} classNumber="input-wrapper"> 
                         <span style= {{ width: `fit-content`, margin: `0`, padding: `1rem 1rem`, display: `flex`, alignItems: `center`,
                                         borderTopLeftRadius: `0.25em`, borderBottomLeftRadius: `0.25em`, borderTopRightRadius: `0`,
@@ -236,7 +581,7 @@ function Profile() {
                                         fontWeight: `400`, lineHeight: `1.5`, color: `#495057`, textAlign: `center`,
                                         backgroundColor: `e9ecef`, }} classNumber="icon">
                             {/* <i style= {{ color: `black`, padding:` 5px`, }} className="fas fa-envelope fa-2x"/> */}
-                            <a href="mailto:scbonner2015@gmail.com" style= {{ color: `black`, padding: `5px`, }} className="fas fa-envelope fa-2x">
+                            {/* <a href="mailto:scbonner2015@gmail.com" style= {{ color: `black`, padding: `5px`, }} className="fas fa-envelope fa-2x">
                             </a>
                             </span>
                                 <input style= {{ flexGrow: `1`, minHeight: `3rem`, padding: `0.375rem 0.75rem`, display: `block`,
@@ -249,9 +594,9 @@ function Profile() {
 
             <div style= {{ width: `100%`, margin: `2rem 0`, position: `relative`, display: `flex`,
                            flexWrap: `wrap`, alignItems: `center`, justifyContent: `flex-start`, }} 
-                           classNumber="fieldset">
+                           classNumber="fieldset"> */}
                 {/* <label style= {{ width: `100%`, margin: `0 0 1rem 1`, fontSize: `1.2rem`, fontWeight: `700`, }} for="user-name"></label> */}
-                    <div style= {{ width: `100%`, display: `flex`, flexFlow: `nowrap`, alignItems: `stetch`, justifyContent: `center`,
+                    {/* <div style= {{ width: `100%`, display: `flex`, flexFlow: `nowrap`, alignItems: `stetch`, justifyContent: `center`,
                                 }} classNumber="input-wrapper"> 
                         <span style= {{ width: `fit-content`, margin: `0`, padding: `1rem 1rem`, display: `flex`, alignItems: `center`,
                                         borderTopLeftRadius: `0.25em`, borderBottomLeftRadius: `0.25em`, borderTopRightRadius: `0`,
@@ -284,7 +629,7 @@ function Profile() {
                       <option value="No" />
                     </datalist>
               </div>
-            </div> 
+            </div>  */}
 {/* 
             <div className="fieldset">
               <div className="input-wrapper"> 
@@ -359,12 +704,12 @@ function Profile() {
               </div>
             </div>  */}
  
-                            <Link to={'/directory/'}>
+                            {/* <Link to={'/directory/'}>
                             <Button> Back to Map  </Button>
-                            </Link>
+                            </Link> */}
                 
-            </form>
-            </div>
+            
+            
             </div>
             </div>        
         </section>
@@ -374,7 +719,7 @@ function Profile() {
     )
  };
 
- 
+};
         
       
       
